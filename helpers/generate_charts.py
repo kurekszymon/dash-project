6bb9b0d4594912b557950cd3f8ff345b49d9b2b9
kurekszymon <contact.kurek@gmail.com>
2022-05-15 10:58:53 +0200

    chore(layout): uppercased const

--- helpers/generate_charts.py
+++ helpers/generate_charts.py
@@ -23,34 +23,34 @@ def generate_table(dataframe):
         ],
     )
 
-provide_more_details = "Define dimension and measure"
+PROVIDE_MORE_DETAILS = "Define dimension and measure"
 
 # maybe add title possibility?
 def generate_bar_chart(dataframe, x_axis, y_axis):
     if not x_axis or not y_axis:
-        return provide_more_details
+        return PROVIDE_MORE_DETAILS
     return px.bar(dataframe, x=x_axis, y=y_axis, barmode="group")
 
 
 def generate_line_chart(dataframe, x_axis, y_axis):
     if not x_axis or not y_axis:
-        return provide_more_details
+        return PROVIDE_MORE_DETAILS
     return px.line(dataframe, x=x_axis, y=y_axis)
 
 
 def generate_scatter_chart(dataframe, x_axis, y_axis):
     if not x_axis or not y_axis:
-        return provide_more_details
+        return PROVIDE_MORE_DETAILS
     return px.scatter(dataframe, x=x_axis, y=y_axis)
 
 
 def generate_heatmap(dataframe, x_axis, y_axis):
     if not x_axis or not y_axis:
-        return provide_more_details
+        return PROVIDE_MORE_DETAILS
     return px.density_heatmap(dataframe, x=x_axis, y=y_axis)
 
 
 def generate_pie_chart(dataframe, x_axis, y_axis):
     if not x_axis or not y_axis:
-        return provide_more_details
+        return PROVIDE_MORE_DETAILS
     return px.pie(dataframe, values=x_axis, names=y_axis)
