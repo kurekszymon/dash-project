1071a253326b89fc8b473ff62a04147515c43e3d
kurekszymon <contact.kurek@gmail.com>
2022-05-23 20:22:33 +0200

    fix: small styles fix

--- components/upload.py
+++ components/upload.py
@@ -6,7 +6,7 @@ def upload_component():
         [
             dcc.Upload(
                 id="upload-data",
-                children=html.Div(["Drag and Drop or ", html.A("Select File")]),
+                children=html.Div(["Select file or drop it here", html.P("(*.csv, *.xls)")]),
             ),
             html.Div(
                 id="output-data-upload",
