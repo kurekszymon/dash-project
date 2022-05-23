4208597e3cd1043e65a707da96ad2eace0a4b597
kurekszymon <contact.kurek@gmail.com>
2022-05-23 20:30:57 +0200

    fix: fixed formatting

--- components/upload.py
+++ components/upload.py
@@ -6,7 +6,9 @@ def upload_component():
         [
             dcc.Upload(
                 id="upload-data",
-                children=html.Div([html.H1("Select file or drop it here"), html.P("(*.csv, *.xls)")]),
+                children=html.Div(
+                    [html.H1("Select file or drop it here"), html.P("(*.csv, *.xls)")]
+                ),
             ),
             html.Div(
                 id="output-data-upload",
